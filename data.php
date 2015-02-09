b1857aa46cbc3ec249ce8dda176e72e34267786d
joydesigner <jason.zhengxin@gmail.com>
2015-02-09 16:40:44 +1100

    Add password validation(at least 4 , alphanumeric.)

--- data.php
+++ data.php
@@ -1,6 +1,7 @@
 <?php
 //password must larger than 3 digits. Blank means anyone can visit.
 $viewpass = '1234';
+$adminpass = '';
 $title = "Jason's Online CV";
 $subtitle = 'Editable online CV easy to use';
 $content = '';
@@ -22,14 +23,17 @@ function show(){
 function update(){
     //get content
     $content = $_POST['content'];
+    $adminpass= trim($_POST['admin_password']);
+    if(strlen($adminpass)>0&& $adminpass!= $viewpass){
+        //update the view pass
+        $viewpass = $adminpass;
+    }
    //write to file
    $file = "content.html";
    file_put_contents($file,$content);
 }
 
 //compare the password
-
-
 if( strlen( $viewpass ) > 0 && trim($_REQUEST['vpass']) != $viewpass )
 {
 	$data['errno'] = '0';
